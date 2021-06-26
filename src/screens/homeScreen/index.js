3b765b8125fe88b18f21f48f9fa024ed2853002a
Amit Roy <amitroy497@yahoo.com>
2021-06-26 23:01:25 +0530

    sixth commit

--- src/screens/homeScreen/index.js
+++ src/screens/homeScreen/index.js
@@ -1,12 +1,18 @@
 import React, { useEffect } from 'react'
+import './_index.scss'
 import { Container, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { getPopularVideos } from '../../redux/actions/videos.action'
 import Categories from './../../components/categories'
 import VideosSection from './../../components/videosSection'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const HomeScreen = () => {
   const dispatch = useDispatch()
+
+  const fetchData = () => {
+    dispatch(getPopularVideos())
+  }
 
   useEffect(() => {
     dispatch(getPopularVideos())
@@ -17,11 +23,18 @@ const HomeScreen = () => {
     <Container>
       <Categories />
       <Row>
-        {videos.map((video, i) => (
-          <Col lg={3} md={4}>
-            <VideosSection video={video} key={video.id} />
-          </Col>
-        ))}
+        <InfiniteScroll
+          dataLength={videos.length}
+          next={fetchData}
+          hasMore={true}
+          loader={<div className='spinner-border text-danger mx-auto'></div>}
+        >
+          {videos.map((video) => (
+            <Col lg={3} md={4}>
+              <VideosSection video={video} key={video.id} />
+            </Col>
+          ))}
+        </InfiniteScroll>
       </Row>
     </Container>
   )
